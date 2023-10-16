b242f9bc07b416e299e5a6080e3a1914f125ad06
kingofdreams777 <rossmartin@sandiego.edu>
2023-10-16 15:43:43 -0700

    finished up about page, css and all

--- pages/about.vue
+++ pages/about.vue
@@ -9,10 +9,12 @@
             </div>
         </div>
     </div>
+
     <div class="hero min-h-screen bg-base-300">
-        <div class="hero-content flex-col lg:flex-row prose">
-            <h1>I received my first Lionel Train set</h1>
-            <div class="prose lg:prose-xl sm:prose-2xl">
+        <div class="hero-content flex-col lg:flex-row">
+            <img src="@/assets/images/trainwithsteamportrait.jpg" alt="Steam Train" class="max-w-sm rounded-lg shadow-2xl">
+            <div class="prose sm:prose-xl">
+                <h1>I received my first Lionel Train set</h1>
                 <p> for Christmas in 1953 when I was eight years old. <em>(I'll let you
                         figure out how old I am now!)</em> It was revered and played with for several years until higher
                     priority things held my interest; like school, cars, etc. Fortunately, my train was kept in the family
@@ -22,23 +24,59 @@
         </div>
     </div>
     <div class="hero min-h-screen bg-base-content">
-        <div class="hero-content flex-col lg:flex-row prose">
+        <div class="hero-content grid prose">
             <h1 class="text-white">1982 came along</h1>
             <div class="prose lg:prose-xl sm:prose-2xl text-white">
                 <p> and we gave our son my train for his seventh birthday. I was interested in doing some
                     minor
                     repair on the locomotive and was introduced to some very knowledgeable people through the local train
                     club
-                    here in San Diego <em>(The All Gauge Toy Train Association)</em> where I learned about model trains. I
+                    here in San Diego
+                    <em>(The All Gauge Toy Train Association)</em> where I learned about model trains. I
                     started buying used items that needed repair and found I enjoyed taking things apart to see how they
-                    worked
-                    <em>(undoubtedly a personality trait carried over from childhood. My father never did forgive me for
-                        disassembling his electric drill.)</em> I thought I should start documenting the repairs and that
-                    progressed to writing helpful repair hints for the club's newsletter. I became friends with the owner of
-                    the
-                    local hobby shop where I would buy old cabooses and used 0-27 track. I offered to be their repairman; it
-                    worked out well for all parties.
+                    worked.
+                <blockquote class="text-white">
+                    <em>Undoubtedly a personality trait carried over from childhood. My father never did forgive me for
+                        disassembling his electric drill.</em>
+                </blockquote>
+                I thought I should start documenting the repairs and that
+                progressed to writing helpful repair hints for the club's newsletter. I became friends with the owner of
+                the
+                local hobby shop where I would buy old cabooses and used 0-27 track. I offered to be their repairman; it
+                worked out well for all parties.
                 </p>
+
+            </div>
+        </div>
+
+    </div>
+    <div class="hero min-h-screen bg-neutral-50">
+        <div class="hero-content flex-col lg:flex-row-reverse">
+            <img src="@/assets/images/boy_aboutme.jpg" alt="James Weatherford Young">
+            <div class="prose sm:prose-xl">
+                <p>Since I was a member of the Toy Train Operating Society, Southern Pacific Division, in Los Angeles, I
+                    began
+                    writing repair hints for their newsletter, the Daylighter. Soon, I was writing longer and more detailed
+                    articles for maintenance and repair of differenting operating cars, accessories, transformers,
+                    locomotives,
+                    and tenders.</p>
+
+                <p>I met Dave Otth through the TTOS and found we had a mutual interest in the production of videos for the
+                    repair of Lionel Model Trains. A partnership was formed where Dave produced the videos and I repaired
+                    trains
+                    in the studio. Together, we produced four videos from 1989 to 1991, volumes 1-3 are 60 minutes each and
+                    volume 4 is 30 minutes.</p>
+
+                <p>My writing and the videos attracted the interest of Dick Christianson, the then editor of the Classic Toy
+                    Trains magazine. He offered me the opportunity to take over the Questions and Answers column for the
+                    magazine which I did from 1993 to July of 1999.</p>
+
+                <p>I've been working on the internet since it first became practical in the early 1980's. It finally evolved
+                    to
+                    a technology that offered unlimited opportunities for information dissemination and that interested me
+                    since
+                    I've always like to share my hobby. That led to creating this site for those interested in fixing and
+                    maintaining toy trains. </p>
 
             </div>
         </div>
@@ -48,24 +86,6 @@
         <div class="text-left prose lg:prose-xl sm:prose-2xl">
 
 
-            <p>Since I was a member of the Toy Train Operating Society, Southern Pacific Division, in Los Angeles, I began
-                writing repair hints for their newsletter, the Daylighter. Soon, I was writing longer and more detailed
-                articles for maintenance and repair of differenting operating cars, accessories, transformers, locomotives,
-                and tenders.</p>
-
-            <p>I met Dave Otth through the TTOS and found we had a mutual interest in the production of videos for the
-                repair of Lionel Model Trains. A partnership was formed where Dave produced the videos and I repaired trains
-                in the studio. Together, we produced four videos from 1989 to 1991, volumes 1-3 are 60 minutes each and
-                volume 4 is 30 minutes.</p>
-
-            <p>My writing and the videos attracted the interest of Dick Christianson, the then editor of the Classic Toy
-                Trains magazine. He offered me the opportunity to take over the Questions and Answers column for the
-                magazine which I did from 1993 to July of 1999.</p>
-
-            <p>I've been working on the internet since it first became practical in the early 1980's. It finally evolved to
-                a technology that offered unlimited opportunities for information dissemination and that interested me since
-                I've always like to share my hobby. That led to creating this site for those interested in fixing and
-                maintaining toy trains. </p>
         </div>
     </div>
 </template>
@@ -77,5 +97,4 @@
     background-position: center center;
     background-repeat: no-repeat;
     background-attachment: fixed;
-}
-</style>
+}</style>
